252b4cba1bde59839ce32fd928d4c5bf42a742ea
Siri S <52484274+sirigowda204@users.noreply.github.com>
2024-09-16 17:16:11 -0400

    Version 1 of portfolio

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from "./sections/Navbar.jsx";
+import Hero from "./sections/Hero.jsx";
+import About from "./sections/About.jsx";
+import Contact from "./sections/Contact.jsx";
+import Footer from "./sections/Footer.jsx";
+import Experience from "./sections/Experience.jsx";
+
+const App = () => {
+    return (
+        <main className="max-w-7xl mx-auto">
+            <Navbar />
+            <Hero />
+            <About />
+            <Experience />
+            <Contact />
+            <Footer />
+        </main>
+    )
+}
+export default App
